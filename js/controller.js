5d09962da78f31d567b5fa1322f5293419ecb09a
sese22 <tienloinguyen22@gmail.com>
2019-04-26 22:01:39 +0700

    update register form + validate

--- js/controller.js
+++ js/controller.js
@@ -1,1 +1,44 @@
 const controller = {};
+
+controller.validateRegisterForm = (registerInfo) => {
+  const { firstName, lastName, email, password, confirmPassword } = registerInfo;
+
+  // validate input
+  if (!firstName) {
+    view.setMessage('firstName-error-message', 'Please input first name');
+  } else {
+    view.setMessage('firstName-error-message', '');
+  }
+
+  if (!lastName) {
+    view.setMessage('lastName-error-message', 'Please input last name');
+  } else {
+    view.setMessage('lastName-error-message', '');
+  }
+
+  // validate email
+  const emailRegex = /^[a-z][a-z0-9_\.]{5,32}@[a-z0-9]{2,}(\.[a-z0-9]{2,4}){1,2}$/;
+  if (emailRegex.test(email)) {
+    view.setMessage('email-error-message', '');
+  } else {
+    view.setMessage('email-error-message', 'Invalid email address');
+  }
+
+  // validate password
+  if (!password) {
+    view.setMessage('password-error-message', 'Please input password');
+  } else if (password.length < 6) {
+    view.setMessage('password-error-message', 'Password must be greater than 6 characters');
+  } else {
+    view.setMessage('password-error-message', '');
+  }
+
+  // validate confirm password
+  if (!confirmPassword) {
+    view.setMessage('confirm-error-message', 'Please confirm password');
+  } else if (confirmPassword !== password) {
+    view.setMessage('confirm-error-message', 'Confirm password didnt match');
+  } else {
+    view.setMessage('confirm-error-message', '');
+  }
+};
--- js/view.js
+++ js/view.js
@@ -1,9 +1,41 @@
 const view = {};
 
-view.setActiveScreen = () => {
-  const app = document.getElementById('app');
+view.setMessage = (elementId, message) => {
+  const element = document.getElementById(elementId);
+  if (element) {
+    element.innerText = message;
+  }
+};
 
-  if (app) {
-    app.innerHTML = components.index;
+view.setActiveScreen = () => {
+  // const app = document.getElementById('app');
+  // if (app) {
+  //   app.innerHTML = components.index;
+  // }
+
+  // Listen submit event
+  const registerForm = document.getElementById('form-wrapper');
+  if (registerForm) {
+    const handleSubmit = (event) => {
+      event.preventDefault();
+
+      // get input values
+      const firstName = registerForm.firstName.value;
+      const lastName = registerForm.lastName.value;
+      const email = registerForm.email.value;
+      const password = registerForm.password.value;
+      const confirmPassword = registerForm.confirmPassword.value;
+
+      const registerInfo = {
+        firstName,
+        lastName,
+        email,
+        password,
+        confirmPassword,
+      };
+
+      controller.validateRegisterForm(registerInfo);
+    };
+    registerForm.addEventListener('submit', handleSubmit);
   }
 };
